b4cf81c7ed4f51a1c7da48305655d8fa241dda01
sydney-pi <sydney.bang94@gmail.com>
2023-11-18 16:17:10 -0500

    creating project in xcode

---
+++ ReaderApp/ReaderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReaderApp
+//
+//  Created by Sydney Bang on 2023-11-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReaderApp/ReaderApp/ReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderAppApp.swift
+//  ReaderApp
+//
+//  Created by Sydney Bang on 2023-11-18.
+//
+
+import SwiftUI
+
+@main
+struct ReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
